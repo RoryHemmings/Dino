86727d1dcc48b6b31a6b4a93efd3c5a6c0e8f62a
RoryHemmings <rpmhemmings@gmail.com>
2019-09-18 23:36:23 -0700

    added wrapper (2)

--- Server/Server.h
+++ Server/Server.h
@@ -1,34 +1,54 @@
-//#pragma once
-//
-//#ifndef SERVER_H
-//#define SERVER_H
-//
-//#include <winsock2.h>
-//#include <ws2tcpip.h>
-//#include <iphlpapi.h>
-//
-//#include <windows.h>
-//#include <stdio.h>
-//
-//class Server
-//{
-//
-//public:
-//	Server() : Server(27015) { }
-//	Server(int port);
-//
-//	SOCKET getSocket() { return listenSocket; }
-//	const SOCKET getSocket() const { return listenSocket; }
-//
-//	int accept();
-//	int recieve();
-//	int close();
-//
-//private:
-//	SOCKET listenSocket, clientSocket;
-//	int port, error;
-//
-//};
-//
-//#endif
-//
+#pragma once
+
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <vector>
+#include <string>
+
+#include <windows.h>
+#include <stdio.h>
+
+#include <SocketWrapper.h>
+
+struct Client {
+	Client(Socket* s) : socket(s), ip("0.0.0.0") { }
+	Client() : socket(NULL), ip("0.0.0.0") { }
+	~Client() { delete socket; }
+
+	Socket* socket;
+	std::string ip;
+};
+
+class Server
+{
+
+public:
+	Server(int port);
+	Server() : Server(DEFAULT_PORT) { }
+
+	// TODO add rule of three
+
+	// Waits for clients
+	void serve();
+
+	// Talks with current connected client
+	void talk();
+
+	void setCurrent(Socket* c) { current = c; }
+
+	void stop() { socket.close(); }
+
+	int port() const { return iPort; }
+
+private:
+	ServerSocket socket;
+	std::vector<Client> clients;
+
+	Client current;
+
+	int iPort;
+};
+
+#endif
+
